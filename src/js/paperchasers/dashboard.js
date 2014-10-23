8e422e517312114dd814ec5b0324283a3c495c52
OpenIntro <eric@openintro.com>
2014-10-23 09:10:53 -0500

    Initial commit

---
+++ src/js/paperchasers/dashboard.js
@@ -0,0 +1,24 @@
+/*
+ * Description:
+ *      This is a demo file used only for the main dashboard (index.html)
+ **/
+
+$(function() {
+    "use strict";
+
+    //Make the dashboard widgets sortable Using jquery UI
+    $(".connectedSortable").sortable({
+        placeholder: "sort-highlight",
+        connectWith: ".connectedSortable",
+        handle: ".box-header, .nav-tabs",
+        forcePlaceholderSize: true,
+        zIndex: 999999
+    }).disableSelection();
+    $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+    
+
+    //The Calender
+    $("#calendar").datepicker();
+
+
+});
